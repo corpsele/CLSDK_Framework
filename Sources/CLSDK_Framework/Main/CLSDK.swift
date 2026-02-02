50d1e74042a04aae0ec5c430be926db0bca7e277
corpsele <corpsele555@gmail.com>
2026-02-02 11:41:30 +0800

    add PublicModel

---
+++ Sources/CLSDK_Framework/Main/CLSDK.swift
@@ -0,0 +1,10 @@
+//
+//  CLSDK.swift
+//  CLSDK_Framework
+//
+//  Created by corpsele_n on 2026/2/2.
+//
+
+public class CLSDK {
+    
+}
---
+++ Sources/CLSDK_Framework/Models/PublicModel.swift
@@ -0,0 +1,46 @@
+//
+//  PublicModel.swift
+//  CLSDK_Framework
+//
+//  Created by corpsele_n on 2026/2/2.
+//  Adapter
+
+protocol PublicModel {
+    var id: String {
+        get
+        set
+    }
+    var title: String { get }
+    var strUrl: String { get }
+}
+
+struct LegacyModel {
+    var id: String
+    let strTitle: String
+    let strUrl: String
+    let subTitle: String
+}
+
+class PublicModelAdapter: PublicModel {
+    
+    var legacyModel: LegacyModel
+    
+    init(legacyModel: LegacyModel) {
+        self.legacyModel = legacyModel
+    }
+    
+    var id: String {
+        get{
+            legacyModel.id
+        }
+        set{
+            legacyModel.id = newValue
+        }
+    }
+    
+    var title: String { "\(legacyModel.strTitle) \(legacyModel.subTitle)" }
+    
+    var strUrl: String { legacyModel.strUrl }
+    
+    
+}
