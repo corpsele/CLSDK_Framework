16819024835267bd566655191b889d850267e462
corpsele <corpsele555@gmail.com>
2026-02-02 12:42:10 +0800

    upgrade

--- Sources/CLSDK_Framework/Main/CLSDK.swift
+++ Sources/CLSDK_Framework/Main/CLSDK.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class CLSDK {
     
-    public func outputModel() {
+    public static func outputModel() {
         let model = PublicModelAdapter(legacyModel: LegacyModel(id: "1", strTitle: "Title", strUrl: "url", subTitle: "subTitle"))
         print("model id = \(model.id), strTitle = \(model.title), strUrl = \(model.strUrl)")
     }
